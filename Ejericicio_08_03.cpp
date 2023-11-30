c49b3d57a17e8ce0d772a83655956277c452561d
JudithRomerCori <142638700+JudithRomerCori@users.noreply.github.com>
2023-11-30 01:21:04 -0400

    Ejericicio_08_03.cpp

--- Ejericicio_08_03.cpp
+++ Ejericicio_08_03.cpp
@@ -1,29 +1,30 @@
-// Materia: Programación I, Paralelo 1
+// Materia: ProgramaciÃ³n I, Paralelo 1
 
 // Autor: Judith Marisol Romero Cori
 
-// Fecha creación: 20/11/2023
+// Fecha creaciÃ³n: 20/11/2023
 
-// Fecha modificación: 25/11/2023
+// Fecha modificaciÃ³n: 25/11/2023
 
-// Número de ejericio: 3
+// NÃºmero de ejericio: 3
 
-// Problema planteado:Amplíe el programa anterior que procesa clientes de una agencia matrimonial para que tome
+// Problema planteado:AmplÃ­e el programa anterior que procesa clientes de una agencia matrimonial para que tome
 //los datos de todos los candidatos a estudiar del fichero PERSONAS.DAT del ejercicio anterior,
 //lea el cliente del teclado y finalmente genere como resultado un listado por pantalla con los
 //nombres de los candidatos aceptados y un fichero llamado ACEPTADOS.DAT con toda la
-//información correspondiente a los candidatos aceptados.
-//Una persona del fichero PERSONAS.DAT se considerará aceptable como candidato si tiene
-//diferente sexo y que haya nacido en el mes y año (El programa debe ser capaz de trabajar con
-//cualquier número de personas en el fichero PERSONAS.DAT).
+//informaciÃ³n correspondiente a los candidatos aceptados.
+//Una persona del fichero PERSONAS.DAT se considerarÃ¡ aceptable como candidato si tiene
+//diferente sexo y que haya nacido en el mes y aÃ±o (El programa debe ser capaz de trabajar con
+//cualquier nÃºmero de personas en el fichero PERSONAS.DAT).
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <ctime>
 
 using namespace std;
 
-const char* NOMBRE_ARCHIVO_PERSONAS = "PERSONAS.BIN";
-const char* NOMBRE_ARCHIVO_ACEPTADOS = "ACEPTADOS.BIN";
+const char* NOMBRE_ARCHIVO_ENTRADA = "PERSONAS.BIN";
+const char* NOMBRE_ARCHIVO_SALIDA = "ACEPTADOS.DAT";
 
 struct Persona
 {
@@ -37,58 +38,66 @@ void escribirArchivoBinario(const Persona& persona, const char* nombreArchivo)
 {
     ofstream archivoEscritura;
     archivoEscritura.open(nombreArchivo, ios::app | ios::binary);
+
+    if (!archivoEscritura.is_open())
+    {
+        cout << "No se pudo abrir el archivo para escritura." << endl;
+        return;
+    }
+
     archivoEscritura.write(reinterpret_cast<const char*>(&persona), sizeof(Persona));
+
     archivoEscritura.close();
+}
+
+bool esAceptable(const Persona& persona)
+{
+    // Obtener la fecha actual para comparar con la fecha de nacimiento
+    time_t tiempoActual = time(nullptr);
+    tm* fechaActual = localtime(&tiempoActual);
+
+    // Obtener el mes y el aÃ±o de la fecha de nacimiento
+    int mesNacimiento, anioNacimiento;
+    sscanf(persona.fechaNacimiento, "%d/%*d/%d", &mesNacimiento, &anioNacimiento);
+
+    // Verificar si el sexo es diferente y la persona naciÃ³ en el mismo aÃ±o y mes actual
+    return (persona.sexo != 'M' && persona.sexo != 'F') ||
+           (persona.sexo != 'M' && mesNacimiento == fechaActual->tm_mon + 1 && anioNacimiento == fechaActual->tm_year + 1900);
 }
 
 void procesarCandidatos()
 {
-    ifstream archivoPersonas;
-    Persona persona;
+    ifstream archivoEntrada(NOMBRE_ARCHIVO_ENTRADA);
+    ofstream archivoSalida(NOMBRE_ARCHIVO_SALIDA, ios::binary);
 
-    archivoPersonas.open(NOMBRE_ARCHIVO_PERSONAS, ios::in | ios::binary);
-
-    if (!archivoPersonas.is_open())
+    if (!archivoEntrada.is_open() || !archivoSalida.is_open())
     {
-        cout << "No se pudo abrir el archivo de personas." << endl;
+        cout << "Error al abrir los archivos." << endl;
         return;
     }
 
-    cout << "\nCandidatos aceptados:" << endl;
+    Persona candidato;
 
-    while (archivoPersonas.read(reinterpret_cast<char*>(&persona), sizeof(Persona)))
+    cout << "Candidatos aceptados:" << endl;
+
+    while (archivoEntrada.read(reinterpret_cast<char*>(&candidato), sizeof(Persona)))
     {
-        // Verificar si la persona es un candidato aceptado
-        if (persona.sexo != 'M' && persona.sexo != 'F')
+        if (esAceptable(candidato))
         {
-            cout << "Error: Sexo no válido para " << persona.nombre << endl;
-            continue;
-        }
-
-        // Obtener el mes y año de nacimiento
-        int mes, anno;
-        sscanf(persona.fechaNacimiento, "%*d/%d/%d", &mes, &anno);
-
-        // Verificar si es un candidato aceptado
-        if (mes % 2 == 0 && anno % 2 != 0)
-        {
-            cout << "Nombre: " << persona.nombre << ", Edad: " << persona.edad << ", Sexo: " << persona.sexo << ", Fecha Nacimiento: " << persona.fechaNacimiento << endl;
-
-            // Escribir la información en el archivo de aceptados
-            escribirArchivoBinario(persona, NOMBRE_ARCHIVO_ACEPTADOS);
+            cout << candidato.nombre << endl;
+            escribirArchivoBinario(candidato, NOMBRE_ARCHIVO_SALIDA);
         }
     }
 
-    archivoPersonas.close();
+    archivoEntrada.close();
+    archivoSalida.close();
 }
 
 int main()
 {
-    // Código para ingresar personas al archivo PERSONAS.BIN (del ejercicio anterior)
-
-    // Llamada para procesar candidatos y generar archivo de aceptados
     procesarCandidatos();
 
     return 0;
 }
+
 
